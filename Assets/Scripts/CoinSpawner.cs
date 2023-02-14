fedb5a2390479d7684cf9567f06fb1ab17e829ce
DaniAndry <danilenkoandrey92@gmail.com>
2023-02-14 12:16:45 +0400

    new

--- Assets/Scripts/CoinSpawner.cs
+++ Assets/Scripts/CoinSpawner.cs
@@ -1,25 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class CoinSpawner : MonoBehaviour
 {
     [SerializeField] private Coin _coin;
 
-    private Vector2 _position;
+    private Vector2 _triggerPosition;
+    private float _spawnPossition = 20f;
 
     private void Start()
     {
-        _position = transform.position;
-        _position.x += 20f;
+        _triggerPosition = transform.position;
+        _triggerPosition.x += _spawnPossition;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.TryGetComponent<Player>(out Player player))
         {
-            Instantiate(_coin, _position, Quaternion.identity);
+            Instantiate(_coin, _triggerPosition, Quaternion.identity);
             GameObject.Destroy(gameObject);
         }
     }
